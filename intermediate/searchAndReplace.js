b7377b72b856acbd9ef6afbdb61d8983e857d141
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2021-08-18 15:12:44 -0400

    added searchAndReplace.js

---
+++ intermediate/searchAndReplace.js
@@ -0,0 +1,20 @@
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+// First argument is the sentence to perform the search and replace on.
+
+// Second argument is the word that you will be replacing (before).
+
+// Third argument is what you will be replacing the second argument with (after).
+
+// Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+
+function myReplace(str, before, after) {
+    return before[0].toUpperCase() === before[0]
+        ? str.replace(before, after[0].toUpperCase() + after.substring(1))
+        : str.replace(before, after);
+}
+
+console.log(
+    myReplace('A quick brown fox jumped over the lazy dog', 'jumped', 'leaped')
+);
+console.log(myReplace('He is Sleeping on the couch', 'Sleeping', 'sitting'));
